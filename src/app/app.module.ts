8be6f9905d2090ef83db6631111127d9611e182b
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-11-14 22:19:28 +0100

    routes implementations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import { youTubeSearchInjectables } from './you-tube-search.injectables';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "contact", component: ContactsComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     SearchBoxComponent,
     SearchResultComponent,
-    YouTubeSearchComponent
+    YouTubeSearchComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent]
--- src/app/you-tube-search/you-tube-search.component.ts
+++ src/app/you-tube-search/you-tube-search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SearchResultModel } from '../searchresult.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-you-tube-search',
@@ -9,34 +10,44 @@ import { SearchResultModel } from '../searchresult.model';
 export class YouTubeSearchComponent {
   loading: boolean = false
   results: SearchResultModel[] | null = null
+  data = null
 
-  // create an array of results with SearchResultModel
-  // results: SearchResultModel[] = [
-  //   {
-  //     id: '1',
-  //     title: 'Title 1',
-  //     description: 'Description 1',
-  //     thumbnailUrl: '',
-  //     videoUrl: ''
-  //   },
-  //   {
-  //     id: '2',
-  //     title: 'Title 2',
-  //     description: 'Description 2',
-  //     thumbnailUrl: '',
-  //     videoUrl: ''
-  //   },
-  //   {
-  //     id: '3',
-  //     title: 'Title 3',
-  //     description: 'Description 3',
-  //     thumbnailUrl: '',
-  //     videoUrl: ''
-  //   }
-  // ]
+  constructor(
+    private http: HttpClient
+  ) {}
 
   updateResults(results: SearchResultModel[]) {
     this.results = results
     console.log("results:", this.results); // uncomment to take a look
+  }
+
+  makePost(): void {
+    this.loading = true 
+    this.http.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1
+      })
+    ).subscribe((data: any) => {
+        console.log('====================================');
+        console.log(data);
+        console.log('====================================');
+       this.data = data 
+       this.loading = false
+    })
+  }
+
+  makeDelete() {
+    this.loading = true
+    this.http.delete(
+      'https://jsonplaceholder.typicode.com/posts/1'
+    )
+    .subscribe((data: any) => {
+      this.data = data 
+      this.loading = false
+    })
+    
   }
 }
